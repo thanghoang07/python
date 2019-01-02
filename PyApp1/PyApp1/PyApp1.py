3302432dc60d5cde1b7978793828991473897644
Hoang Duc Thang <thanghoang07@gmail.com>
2019-01-02 18:19:18 +0700

    update 3

--- PyApp1/PyApp1/PyApp1.py
+++ PyApp1/PyApp1/PyApp1.py
@@ -1,4 +1,27 @@
-import sys
-from math import cos, radians
-for i in range(360):
-    print(cos(radians(i)))
+#from math import cos, radians
+
+## Create a string with spaces proportional to a cosine of x in degrees
+#def make_dot_string(x):
+#    rad = radians(x)                             # cos works with radians
+#    numspaces = int(20 * cos(radians(x)) + 20)   # scale to 0-40 spaces
+#    st = ' ' * numspaces + 'o'                   # place 'o' after the spaces
+#    return st
+
+#def main():
+#    for i in range(0, 1800, 12):
+#        s = make_dot_string(i)
+#        print(s)
+
+#main()
+
+from math import radians
+import numpy as np     # installed with matplotlib
+import matplotlib.pyplot as plt
+
+def main():
+    x = np.arange(0, radians(1800), radians(12))
+    plt.plot(x, np.cos(x), 'b')
+    plt.show()
+
+main()
+
